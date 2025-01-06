d26ce06260073bb6ccec09f3b81507db5559d6e0
Gabriel da Costa Rodrigues <167185964+GRodrigues10@users.noreply.github.com>
2025-01-06 14:30:36 -0300

    Aula 6

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const porta = process.env.PORT;
+
+app.get('/', (req, res)=>{
+    res.send('Seja Bem-Vindo');
+    return res.end();
+});
+
+app.listen(porta || 3000,()=>{
+console.log('Rodando!');
+})
